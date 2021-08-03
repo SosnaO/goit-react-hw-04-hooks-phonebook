9897992c58fb8ba98f27c7aef711bdc93ab0a0dd
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-08-03 17:43:38 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import ContactList from './components/ContactList/ContactList'
+import ContactForm from './components/ContactForm/ContactForm'
+import Filter from './components/Filter/Filter'
+import styles from './components/ContactForm/ContactForm.module.css'
+    export default function App(){
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] =useState('');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  useEffect(() => {
+    if (localStorage.getItem('contacts')) {
+      setContacts(JSON.parse(localStorage.getItem('contacts')));
+    }
+  }, []);
+
+  useEffect(
+    prevState => {
+      if (contacts !== prevState) {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+      }
+    },
+    [contacts],
   );
-}
+    const changeFilter = element => {
+    setFilter(element.currentTarget.value);
+  };
+  const deleteContacts = contactId =>{
+    setContacts(() =>(
+    contacts.filter((contact)=>contact.id !==contactId)
+    ));
+  };
+   const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase()),
+  );
+  const formSubmitHandler = data => {
+    if (contacts.find(contact => contact.name === data.name)) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+    setContacts(prevState => [data, ...prevState]);
+  };
 
-export default App;
+  return (
+    <div className={styles.phonebook}>
+    <h1>Phonebook</h1>
+    <ContactForm
+    onSubmit={formSubmitHandler}
+    />
+    <h2>Contacts</h2>
+  <Filter value={filter} onChange={changeFilter} />
+  <ContactList
+  contacts={visibleContacts}
+  onChange={changeFilter}
+  onDelete={deleteContacts} /> 
+  </div>
+  );
+  }
